3497e1e49bd341113c33fa6d8c3bf04842e7ff11
Eflukx <rogier@lodewijks.org>
2015-05-11 22:25:19 +0200

    Small update of the example

--- example/example.rb
+++ example/example.rb
@@ -3,14 +3,27 @@ require 'serialport'
 require '../lib/enocean'
 require './enoceanapp'
 
+unless ARGV[0]
+  puts "Please invoke as '#{$PROGRAM_NAME} [TCM310 serial port/dev]'"
+  exit -1
+end
+
 module Enocean
   config_file = 'devices.yml'
   app = EnoceanApp.new SerialPort.new(ARGV[0], 57600)
 
   trap("SIGINT") do
+    puts "\nBye now..."
     app.stop
     exit
   end
+
+  trap("INT") do
+    puts "\nBye now..."
+    app.stop
+    exit
+  end
+
 
   $known_devices = File.exists?(config_file) ? YAML.load_file(config_file) : {}
 
@@ -20,10 +33,10 @@ module Enocean
   # Esp3::Radio.rorg_codes[pak.rorg].create pak.eep.eep_class::Request.new(32).data
 
   incoming_esp_packets = []
+  outgoing_queue = {}
 
   handle_incoming_packet = ->(packet){
     puts "handling packet..."
-
   }
 
   add_device_to_known_devices = ->(packet){
@@ -40,6 +53,16 @@ module Enocean
     incoming_esp_packets << packet
 
     add_device_to_known_devices.(packet) if packet.learn?
+
+    # do we have something to send?
+    # Enocean::Eep::A5_20_01::Request.new(32).esp_packet.serialize
+    key = packet.sender_id.to_s
+    if outpacket = outgoing_queue[key]
+      outpacket.serialize
+
+    end
+
+
 
   end
 
