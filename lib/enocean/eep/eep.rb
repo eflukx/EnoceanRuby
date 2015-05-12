ca18f07cdcf2820e3e4067d1d888c5eb44ebc550
Eflukx <rogier@lodewijks.org>
2015-05-12 21:33:23 +0200

    Added A5-20-01 Response class.

--- lib/enocean/eep/eep.rb
+++ lib/enocean/eep/eep.rb
@@ -7,7 +7,6 @@ module Enocean
 
     class A5_20_01 < Eep
 
-
       def self.description
         "Battery Powered Actuator"
       end
@@ -24,10 +23,74 @@ module Enocean
         data.hex_join '-'
       end
 
+      def learn?
+        (@data[0] & 0x8) == 0 ? true : false
+      end
+
       class Response < A5_20_01
+
+        attr_accessor :data
+
         def initialize data
           @data = Enocean::Tools.to_bytearray data, 4
         end
+
+        def current_value
+          @data[0]
+        end
+
+        def temperature
+          (@data[2]*40)/255.0
+        end
+
+        def service_on
+          (@data[1] & 0x80) >> 7
+        end
+
+        def energy_input_enabled
+          (@data[1] & 0x40) >> 6
+        end
+
+        def energy_storage
+          (@data[1] & 0x20) >> 5
+        end
+
+        def battery_capacity
+          (@data[1] & 0x10) >> 4
+        end
+
+        def contact_open
+          (@data[1] & 0x8) >> 3
+        end
+
+        def out_of_range
+          (@data[1] & 0x4) >> 2
+        end
+
+        def window_open
+          (@data[1] & 0x2) >> 1
+        end
+
+        def actuator_obstructed
+          @data[1] & 0x1
+        end
+
+        def to_s
+        %Q\
+*** Data of #{self.class} ***
+current_value         : #{current_value}
+temperature           : #{"%.2f" % temperature}
+service_on            : #{service_on}
+energy_input_enabled  : #{energy_input_enabled}
+energy_storage        : #{energy_storage}
+battery_capacity      : #{battery_capacity}
+contact_open          : #{contact_open}
+out_of_range          : #{out_of_range}
+window_open           : #{window_open}
+actuator_obstructed   : #{actuator_obstructed}
+\
+        end
+
       end
 
       class Request < A5_20_01
@@ -79,6 +142,7 @@ module Enocean
           (sp < 0 ? 0 : sp).truncate
         end
 
+        # This class has wrong data layout to be a learn packet (should contain EEP info, no operational data)
         def no_learn
           8
         end
